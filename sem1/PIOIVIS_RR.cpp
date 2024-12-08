ced278bbf77d07f49b93c9fdf909b988a0b78a31
fame <ef188209@gmail.com>
2024-12-08 15:19:01 +0300

    Update PIOIVIS_RR.cpp

--- sem1/PIOIVIS_RR.cpp
+++ sem1/PIOIVIS_RR.cpp
@@ -1,45 +1,111 @@
 #include <iostream>
-using namespace std;
+#include <vector>
+#include <set>
 
-// 3.6 матрица инцидентности для неопределенного графа
+using namespace std;
 
 int main()
 {
     int n, m;
-    cout << "Введите количество вершин: ";
-    cin >> n;
-    cout << "Введите количество рёбер: ";
-    cin >> m;
+    cout << "Введите количество вершин и рёбер: ";
+    cin >> n >> m;
 
-    int graph[m][2]; // для одного ребра пара вершин
-
-    cout << "Введите рёбра (пары вершин):\n";
-    for (int i = 0; i < m; i++)
+    vector<vector<int>> matrix(n, vector<int>(m));
+    cout << "Введите матрицу инцидентности (n x m):\n";
+    for (int i = 0; i < n; i++)
     {
-        cin >> graph[i][0] >> graph[i][1]; // 0-0  0-1   1-0  1-1
+        for (int j = 0; j < m; j++)
+        {
+            cin >> matrix[i][j];
+        }
     }
 
-    int mat[m][n];
-
-    for (int i = 0; i < m; i++)
+    vector<vector<int>> graph(n, vector<int>(n, 0)); // n - кол-во вершин, 0 -не связаны
+    for (int j = 0; j < m; j++)
     {
-        for (int j = 0; j < n; j++)
+        int u = -1, v = -1;         // индексы двух  вершин (-1 -- не найдены)
+        for (int i = 0; i < n; i++) // проходим по ребру
         {
-            mat[i][j] = 0;
+            if (matrix[i][j] == 1)
+            {
+                if (u == -1)
+                    u = i;
+                else
+                    v = i;
+            }
         }
-
-        mat[i][graph[i][0] - 1] = 1; // единичку в нужный столбик
-        mat[i][graph[i][1] - 1] = 1;
+        if (u != -1 && v != -1)
+        {
+            graph[u][v] = 1;
+            graph[v][u] = 1; // неориентированный
+        }
     }
 
-    cout << "\nМатрица инцидентности для графа:\n";
-    for (int i = 0; i < m; i++)
+    set<pair<int, int>> unionGraph; // тут  все ребра гамильтонова цикла
+    vector<int> path;               // тут все вершины которые мы прошли
+    vector<bool> visited(n, false); // проверка проходили мы вершины
+    bool cycleFound = false;        // найден ли гам. цикл
+
+    for (int start = 0; start < n; start++)
     {
-        for (int j = 0; j < n; j++)
+        path.clear();                                // очищаем
+        fill(visited.begin(), visited.end(), false); // чтобы все вершины не пройдены
+        visited[start] = true;                       // стартовая строка - посетили
+        path.push_back(start);                       // добавили в путь
+
+        vector<int> stack = {start}; // для поиска соседей (Берём вершину 3 из стека (она на верхушке), ищем её соседей (0 и 2). Вершина 0 уже посещена, добавляем 2 в стек: Берём вершину 2 из стека, ищем её соседей (1 и 3). Вершина 3 уже посещена, 1 тоже, процесс завершён.)
+
+        while (!stack.empty()) // пока есть элементы
         {
-            cout << mat[i][j] << " ";
+            int vertex = stack.back(); // берем верхушку(вершина которая добавлена в стек)
+            stack.pop_back();          // удаляем ее чтобы
+
+            if (path.size() == n) // проверяем прошли ли мы по всем путям
+            {
+                if (graph[path.back()][path[0]] == 1) // проверяем сущ. ли ребро соед. последнюю вершину с начальной
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        int u = path[i];           // текущая вершина
+                        int v = path[(i + 1) % n]; // след вершина
+                        if (u > v)                 // сортируем чтобы избегать дубликатов
+                            swap(u, v);
+                        unionGraph.insert({u, v}); // добавляем, инсерт не дает добавить дубликаты
+                    }
+                    cycleFound = true; // найден гам. цикл
+                }
+                continue;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                if (!visited[i] && graph[vertex][i] == 1) // проверяем можно ли переместиться в вершину
+                {
+                    visited[i] = true;
+                    path.push_back(i);  // добавляем в путь
+                    stack.push_back(i); // добавляем в стек
+                    break;
+                }
+            }
+            if (stack.size() == path.size()) // если одинаковое количество вершин то нету новых для пути, удаляем последнюю вершину
+            {
+                path.pop_back();
+            }
         }
-        cout << endl;
+    }
+
+    // Вывод объединённого графа
+    cout << "Рёбра объединённого графа: " << endl;
+    if (!cycleFound)
+    {
+        cout << "Нет рёбер в объединённом графе" << endl;
+    }
+    else
+    {
+        for (auto &edge : unionGraph)
+        {
+            cout << edge.first + 1 << " - " << edge.second + 1 << endl; // Сдвиг на 1 для вывода
+        }
     }
 
     return 0;
